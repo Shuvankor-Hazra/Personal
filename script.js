6f97d0984635e988e6806c896198b681a7542950
Shuvankor-Hazra <shuvo.s.ovuhs@gmail.com>
2023-01-14 13:02:37 +0600

    All Compleated

--- script.js
+++ script.js
@@ -21,13 +21,13 @@ submitButton.addEventListener("click", function () {
     nameField.value = "";
     emailField.value = "";
     messageField.value = "";
-    console.log(nameValue, emailValue, messageValue);
-    // emailjs.send(
-    //   "service_test",
-    //   "template_ukn2w4o",
-    //   templateParams,
-    //   "3xd1VyeW9RbKVVGor"
-    // );
+    // console.log(nameValue, emailValue, messageValue);
+    emailjs.send(
+      "service_test",
+      "template_ukn2w4o",
+      templateParams,
+      "3xd1VyeW9RbKVVGor"
+    );
   } else {
     errorDiv.innerHTML = "<p>Please fill all empty box</p>";
   }
