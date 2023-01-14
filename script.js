8982a9d9e2ea9f0f4f85d22d814852c489aa8e3e
Shuvankor-Hazra <shuvo.s.ovuhs@gmail.com>
2023-01-14 13:00:49 +0600

    All Compleate

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ submitButton.addEventListener("click", function () {
   const nameField = document.getElementById("contactName");
   const emailField = document.getElementById("contactEmail");
   const messageField = document.getElementById("contactMessage");
+  const errorDiv = document.getElementById("error-div");
 
   const nameValue = nameField.value;
   const emailValue = emailField.value;
@@ -16,12 +17,18 @@ submitButton.addEventListener("click", function () {
   };
 
   if (nameValue && emailValue && messageValue) {
+    errorDiv.innerHTML = "";
+    nameField.value = "";
+    emailField.value = "";
+    messageField.value = "";
     console.log(nameValue, emailValue, messageValue);
-    emailjs.send(
-      "service_test",
-      "template_ukn2w4o",
-      templateParams,
-      "3xd1VyeW9RbKVVGor"
-    );
+    // emailjs.send(
+    //   "service_test",
+    //   "template_ukn2w4o",
+    //   templateParams,
+    //   "3xd1VyeW9RbKVVGor"
+    // );
+  } else {
+    errorDiv.innerHTML = "<p>Please fill all empty box</p>";
   }
 });
